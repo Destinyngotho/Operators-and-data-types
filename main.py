72b18411125adb6f80306f7794f1a38f29bc7078
DESTINY WANJIRU <dwngotho@gmail.com>
2024-11-28 10:46:51 +0300

    so

---
+++ main.py
@@ -0,0 +1,20 @@
+# a=True
+# print(type(a))
+# b=5
+# print(type(b))
+# c='hello'
+# print(type(c))
+# d=8.627
+# print(type(d))
+# a='5'
+# b='6'
+# print(int(a)+int(b))
+m=int(input("Enter the number1:"))
+
+n=int(input("Enter the number2:"))
+
+print("Quotient:",m//n)
+
+print("Reminder:",m%n)
+
+print("Answer in decimal:",m/n)
